9eaf241191163f34df6832d0efd5cd8cb92b165b
hawdi <hawdi@pop-os.localdomain>
2019-05-04 20:40:19 +0430

    Upload Files

---
+++ GameLogger/src/gamelogger/FirstInteractionLayoutController.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamelogger;
+
+import gamelogger.database.SQLquerries;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author hawdi
+ */
+public class FirstInteractionLayoutController implements Initializable {
+
+    //UI components and variables
+    @FXML
+    private ListView<String> gameList;
+    @FXML
+    private TextField gameNameTxt;
+    @FXML
+    private Button addGameButton;
+    @FXML
+    private Button exitButton;
+    @FXML
+    private TitledPane recentPane;
+    @FXML
+    private Button continueButton;
+    
+    private ObservableList<String> names = FXCollections.observableArrayList();
+    
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        continueButton.setDisable(true); //disable the continue button at startup
+        MenuItem delete = new MenuItem("Delete");   //create context menu item for list context menu
+        delete.setOnAction((ActionEvent event) -> { //action will give the selected item and removes it
+            try{
+                names.remove(gameList.getSelectionModel().getSelectedIndex());
+            }catch(ArrayIndexOutOfBoundsException e){
+                System.err.println("nothing for delete");   //if nothing selected for delete
+            }
+            gameList.setItems(names);   //refresh the list for showing remaining items
+        });
+        gameNameTxt.addEventHandler(KeyEvent.KEY_PRESSED, (KeyEvent event) -> {
+            if(event.getCode() == KeyCode.ENTER){
+                names.add(gameNameTxt.getText());
+                gameList.setItems(names);
+                gameNameTxt.setText(null);
+            }
+        });
+        //add right click menu to the list view
+        gameList.setContextMenu(new ContextMenu(delete));
+        //add change listener to the 'name' list for enabling contniue button when user adds a game to the list.
+        //otherwise if list is empty, the continue button is set to disable again
+        names.addListener((ListChangeListener.Change<? extends String> c) -> {
+            if(names.isEmpty()){
+                continueButton.setDisable(true);
+            }
+                else{
+                continueButton.setDisable(false);
+            }
+        });
+    }    
+    
+    @FXML
+    void addGameButtonActionPerformed(ActionEvent event) {
+        names.add(gameNameTxt.getText());
+        gameList.setItems(names);
+        gameNameTxt.setText(null);
+    }
+
+    @FXML
+    void continueButtonActionPerformed(ActionEvent event) {
+        //insert entered games to the list
+        SQLquerries querry = new SQLquerries();
+        //insert each gameList item in the database
+        gameList.getItems().forEach((item) -> {
+            querry.insertNewGame(item);
+        });
+        
+        AnchorPane mainWindow;
+        try{
+            mainWindow = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+            Scene scene = new Scene(mainWindow);
+            Stage mwstage = new Stage();
+            mwstage.setTitle("Game Logger");
+            mwstage.setMinWidth(500);
+            mwstage.setMinHeight(360);
+            mwstage.setScene(scene);
+            mwstage.show(); //showing main window
+
+            //closing firstInteraction window
+            Stage current = (Stage) continueButton.getScene().getWindow();
+            current.close();
+        }catch(IOException e){
+            e.getMessage();
+        }
+    }
+
+    @FXML
+    void exitButtonActionPerformed(ActionEvent event) {
+        System.exit(0);
+    }
+    
+}
---
+++ GameLogger/src/gamelogger/GameLogger.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gamelogger;
+
+import gamelogger.database.SQLquerries;
+import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author hawdi
+ */
+public class GameLogger extends Application {
+
+    private boolean firstTime;
+
+    @Override
+    public void start(Stage primaryStage) {
+        AnchorPane root = null;
+        //We check in configuration table in the database is set to 1, it means it's the first runtime of the app here. otherwise we call the default application window
+        SQLquerries querry = new SQLquerries();
+        Map<String, Integer> conf = querry.getUserConfig();
+        //check if the program is first time running
+        if (conf.isEmpty() || conf.get("firstInteraction") == 1) {
+
+            try {
+                root = FXMLLoader.load(getClass().getResource("FirstInteractionLayout.fxml"));
+            } catch (IOException ex) {
+                Logger.getLogger(GameLogger.class.getName()).log(Level.SEVERE, null, ex);
+                System.err.println(ex.getMessage());
+            }
+            querry.insertUserConfig("firstInteraction", 0);
+            Scene scene = new Scene(root);
+
+            primaryStage.setTitle("Game Logger");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+//        primaryStage.setMinWidth(400);
+//        primaryStage.setMinHeight(300);
+            primaryStage.show();
+        }
+        //if the program is not in first run
+        else{
+            try{
+                root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+                Scene scene = new Scene(root);
+                primaryStage.setTitle("Game Logger");
+                primaryStage.setScene(scene);
+                primaryStage.setResizable(false);
+                primaryStage.show();
+            } catch (IOException ex) {
+                Logger.getLogger(GameLogger.class.getName()).log(Level.SEVERE, null, ex);
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
