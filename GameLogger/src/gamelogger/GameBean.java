54d1fbdcd7a22ba8277c69458ce02cbe98d6fc20
hadinajfi77 <hadinajafi.77@gmail.com>
2019-05-05 11:16:32 +0430

    add duration formatting

--- GameLogger/src/gamelogger/GameBean.java
+++ GameLogger/src/gamelogger/GameBean.java
@@ -1,7 +1,8 @@
 
 package gamelogger;
 
-import javafx.beans.property.SimpleIntegerProperty;
+import java.util.HashMap;
+import java.util.Map;
 import javafx.beans.property.SimpleStringProperty;
 
 /**
@@ -11,11 +12,12 @@ import javafx.beans.property.SimpleStringProperty;
 public class GameBean {
     private SimpleStringProperty name;
     private SimpleStringProperty date;
-    private SimpleIntegerProperty duration;
+    private SimpleStringProperty duration;
+    public static Map<String, Integer> formattedValues = new HashMap<>();
     
-    public GameBean(String name, Integer duration, String date){
+    public GameBean(String name, int duration, String date){
         this.name = new SimpleStringProperty(name);
-        this.duration = new SimpleIntegerProperty(duration);
+        this.duration = new SimpleStringProperty(formattedDuration(duration));
         this.date = new SimpleStringProperty(date);
     }
     
@@ -23,11 +25,29 @@ public class GameBean {
         return name.get();
     }
     
-    public Integer getDuration(){
+    public String getDuration(){
         return duration.get();
     }
     
     public String getDate(){
         return date.get();
+    }
+    
+    private String formattedDuration(int duration){
+        String formattedDuration;
+        if(duration <= 0)
+            formattedDuration = "0";
+        else if(duration < 60)
+            formattedDuration = duration + " minutes";
+        else{
+            int result = duration / 60;
+            int remains = duration % 60;
+            if(remains != 0)
+                formattedDuration = result + " hour(s) & " + remains + " minutes";
+            else
+                formattedDuration = result + " hour(s)";
+        }
+        formattedValues.put(formattedDuration, duration);
+        return formattedDuration;
     }
 }
