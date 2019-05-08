faef417fbf8e33a7596deca942b41e9ea2d93cc3
hadinajfi77 <hadinajafi.77@gmail.com>
2019-05-08 23:16:21 +0430

    add doc

--- GameLogger/src/gamelogger/AddRecordLayoutController.java
+++ GameLogger/src/gamelogger/AddRecordLayoutController.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * This class is controller for AddrecordLayout.fxml
+ * The UI contains contains Grid Pane and one HBox
+ * Grid Pane contains ComboBox, Spinner, DatePicker
+ * HBox contains two cancel and OK buttons.
+*/
 package gamelogger;
 
 import gamelogger.database.SQLquerries;
